53e2eea837260ee7e4b45a8fd966380f72f7f235
Collin Pesicka <collin@defenseunicorns.com>
2024-05-09 12:53:37 -0400

    feat(inspect): Added --list-images option and YAML bundle reading capability

--- src/test/e2e/commands_test.go
+++ src/test/e2e/commands_test.go
@@ -15,6 +15,8 @@ import (
 	"strings"
 	"testing"
 
+	"gopkg.in/yaml.v2"
+
 	"github.com/defenseunicorns/pkg/helpers"
 	"github.com/defenseunicorns/uds-cli/src/config"
 	"github.com/defenseunicorns/zarf/src/pkg/message"
@@ -102,6 +104,16 @@ func inspectLocalAndSBOMExtract(t *testing.T, tarballPath string) {
 	_, err = os.Stat(config.BundleSBOM)
 	require.NoError(t, err)
 	err = os.RemoveAll(config.BundleSBOM)
+	require.NoError(t, err)
+}
+
+func inspectBundleYAML(t *testing.T, yamlPath string) {
+	cmd := strings.Split(fmt.Sprintf("inspect %s", yamlPath), " ")
+	_, _, err := e2e.UDS(cmd...)
+	data, err := os.ReadFile(yamlPath)
+	require.NoError(t, err)
+	var bundleData map[string]interface{}
+	err = yaml.Unmarshal(data, &bundleData)
 	require.NoError(t, err)
 }
 
--- src/types/options.go
+++ src/types/options.go
@@ -42,6 +42,7 @@ type BundleInspectOptions struct {
 	Source        string
 	IncludeSBOM   bool
 	ExtractSBOM   bool
+	ExtractImages bool
 }
 
 // BundlePublishOptions is the options for the bundle.Publish() function
